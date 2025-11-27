1c1d8fd34344a12b104a7f321799cda4455a50f4
chinmayee123456 <danyashrichinu@gmail.com>
2025-11-27 14:46:08 +0530

    Refactor input handling and average calculation

--- detail.py
+++ detail.py
@@ -1,30 +1,36 @@
 import sys
-if len(sys.argv)!=6:
-  script_name=sys.argv[0]
-  sub_1=sys.argv[1]
-  sub_2=sys.argv[2]
-  sub_3=sys.argv[3]
-  sub_4=sys.argv[4]
-  sub_5=sys.argv[5]
-  print("user provided input values:")
+
+if len(sys.argv) == 6:
+    # User provided values
+    script_name = sys.argv[0]
+    sub_1 = float(sys.argv[1])
+    sub_2 = float(sys.argv[2])
+    sub_3 = float(sys.argv[3])
+    sub_4 = float(sys.argv[4])
+    sub_5 = float(sys.argv[5])
+
+    print("User provided input values:")
+
 else:
-  script_name=sys.argv[0]
-  sub_1=90
-  sub_2=80
-  sub_3=65
-  sub_4=76
-  sub_5=50
-  print("No input given-using default values:")
-  avg=(sub_1+sub_2+sub_3+sub_4+sub_5)/100
-  print("average marks is:",avg)
-  print("grade is:",grade)
-  if(avg>=80):
-    grade="A"
-  elif(avg>=70):
-    grade="B"
-  elif(avg>=60):
-    grade="C"
-  elif(avg>=40):
-    grade="D"
-  else:
-    grade:fail
+    # Default values
+    script_name = sys.argv[0]
+    sub_1 = 90
+    sub_2 = 80
+    sub_3 = 65
+    sub_4 = 76
+    sub_5 = 50
+  print("No input given - using default values:")
+avg = (sub_1 + sub_2 + sub_3 + sub_4 + sub_5) / 5
+print("Average marks is:", avg)
+if avg >= 80:
+    grade = "A"
+elif avg >= 70:
+    grade = "B"
+elif avg >= 60:
+    grade = "C"
+elif avg >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("Grade is:", grade)
